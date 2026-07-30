015ff156a8b5e33be5512134ef5304434152291b
Gjorgdy <jordy.reins@proton.me>
2026-07-30 16:32:19 +0200

    26.2 FIX: jukebox vanilla note particles

---
+++ src/main/java/nl/gjorgdy/sculk_radio/mixins/JukeboxSongPlayerMixin.java
@@ -0,0 +1,24 @@
+package nl.gjorgdy.sculk_radio.mixins;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.JukeboxSongPlayer;
+import net.minecraft.world.level.LevelAccessor;
+import nl.gjorgdy.sculk_radio.objects.nodes.audio.RadioNode;
+import nl.gjorgdy.sculk_radio.utils.NodeUtils;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(JukeboxSongPlayer.class)
+public class JukeboxSongPlayerMixin {
+
+	@Inject(method = "spawnMusicParticles",
+			at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerLevel;sendParticles(Lnet/minecraft/core/particles/ParticleOptions;DDDIDDDD)I"), cancellable = true)
+	private static void onSpawnMusicParticles(LevelAccessor level, BlockPos blockPos, CallbackInfo ci) {
+		if (level.isClientSide()) return;
+		var node = NodeUtils.getFromBlockEntity(level.getBlockEntity(blockPos.above()));
+		if (node instanceof RadioNode) ci.cancel();
+	}
+
+}
